73600b5f16c777f7fbee2b669a96e960762ecef8
João Teodoro <joaotdn@gmail.com>
2022-10-13 03:51:08 -0300

    implements search by name

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,16 @@
   <div class="companies-app">
     <top-bar />
     <div class="container">
+      <div class="filters">
+        <search id="search-company" v-model:name="name" @search-company="getByName" />
+      </div>
       <div v-for="company in companies" :key="company.id" class="col">
-        <card
-          :title="company.name"
-          :description="company.description"
-          :email="company.email"
-        />
+        <card :title="company.name" :description="company.description" :email="company.email" />
       </div>
       <div class="row">
-        <button href="#" class="button" :disabled="currentPage > lastPage" @click="listCompanies">Carregar mais</button>
+        <button href="#" class="button" :disabled="currentPage > lastPage" @click="pagination">Carregar mais</button>
       </div>
-    </div> 
+    </div>
   </div>
 </template>
 
@@ -21,34 +20,52 @@ import { ref, onMounted } from 'vue';
 import http from './http';
 import TopBar from './components/TopBar.vue';
 import Card from './components/Card.vue';
+import Search from './components/Search.vue';
 
 export default {
   name: 'companiesApp',
   components: {
     TopBar,
+    Search,
     Card
   },
   setup() {
     let companies = ref([]);
     let lastPage = ref();
     let currentPage = ref(1);
+    let name = ref(null);
 
     const listCompanies = () => {
       http.get('/companies', {
         params: {
-          page: currentPage.value,
-          state: null,
-          city: null
+          page: currentPage.value
         }
       })
         .then(res => res.data)
         .then(res => {
           res.data.forEach((obj) => companies.value.push(obj));
-          lastPage.value  = res.meta.last_page;
-          if (currentPage.value <= lastPage.value) {
-            currentPage.value++;
-          }
+          lastPage.value = res.meta.last_page;
         });
+    };
+
+    const getByName = () => {
+      http.get('/companies', {
+        params: {
+          name: name.value,
+        }
+      })
+        .then(res => res.data)
+        .then(res => {
+          companies.value = res.data;
+          lastPage.value = 0;
+        });
+    };
+
+    const pagination = () => {
+      if (currentPage.value <= lastPage.value) {
+        currentPage.value++;
+        listCompanies();
+      }
     };
 
     onMounted(() => {
@@ -59,7 +76,10 @@ export default {
       companies,
       lastPage,
       currentPage,
-      listCompanies
+      name,
+      listCompanies,
+      pagination,
+      getByName
     }
   }
 }
@@ -67,16 +87,19 @@ export default {
 
 <style scoped lang="scss">
 @import './assets/scss/variables';
+
 .container {
   width: 100%;
   max-width: 740px;
   margin: 0 auto;
+  margin-bottom: 40px;
   background-color: $white;
   border-radius: 8px;
   padding: $gutter;
   display: flex;
   flex-wrap: wrap;
-  > .col {
+
+  >.col {
     width: 100%;
     padding: $gutter;
 
@@ -86,10 +109,11 @@ export default {
     }
   }
 
-  > .row {
+  >.row {
     padding: $gutter;
     text-align: center;
     width: 100%;
+
     button {
       border: none;
       border-radius: 4px;
@@ -103,11 +127,20 @@ export default {
       display: inline-block;
       cursor: pointer;
       transition: background-color 0.3s linear;
+
       &:hover {
         background-color: darken($success, 5%);
       }
-      &[disabled] {display: none;}
+
+      &[disabled] {
+        display: none;
+      }
     }
+  }
+
+  .filters {
+    padding: $gutter;
+    width: 100%;
   }
 }
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="form-control">
+        <label :for="id">Buscar</label>
+        <input type="search" :name="id" :id="id" placeholder="Digite o nome" :value="name"
+            @input="$emit('update:name', $event.target.value)">
+        <button type="button" @click="$emit('searchCompany')" :disabled="!name">
+            <img :src="searchIcon" alt="Ícone de busca">
+        </button>
+    </div>
+</template>
+
+<script setup>
+import searchIcon from '../assets/search.png'
+defineProps(['name', 'id']);
+defineEmits(['update:name', 'searchCompany']);
+</script>
+<style scoped lang="scss">
+@import '../assets/scss/components/form-controls';
+</style>
