9d3327bb489bbf59c1916e71c4ac3b2d8989a22d
João Teodoro <joaotdn@gmail.com>
2022-10-12 21:26:23 -0300

    list companies cards

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <div class="companies-app">
     <top-bar />
-    <div class="container"></div> 
+    <div class="container">
+      <div v-for="company in companies" :key="company.id" class="col">
+        <card
+          :title="company.name"
+          :description="company.description"
+          :email="company.email"
+        />
+      </div>
+    </div> 
   </div>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue';
+import http from './http';
 import TopBar from './components/TopBar.vue';
+import Card from './components/Card.vue';
 
 export default {
   name: 'companiesApp',
   components: {
-    TopBar
+    TopBar,
+    Card
+  },
+  setup() {
+    let companies = ref();
+
+    onMounted(() => {
+      http.get('/companies')
+        .then(res => res.data)
+        .then(res => companies.value = res.data);
+    });
+
+    return {
+      companies
+    }
   }
 }
 </script>
@@ -24,6 +49,17 @@ export default {
   margin: 0 auto;
   background-color: $white;
   border-radius: 8px;
-  padding: calc($gutter *2) $gutter;
+  padding: $gutter;
+  display: flex;
+  flex-wrap: wrap;
+  > .col {
+    width: 100%;
+    padding: $gutter;
+
+    @media screen and (min-width: 741px) {
+      width: 50%;
+      float: left;
+    }
+  }
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="card">
+        <h2 class="card__title">{{ title }}</h2>
+        <p class="card_description">{{ description }}</p>
+        <div class="card__links">
+            <a :href="`mailto:${email}`">Enviar email</a>
+            <a href="#">Whatsapp</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props: {
+        title: {
+            type: String,
+            default: null
+        },
+        description: {
+            type: String,
+            default: null
+        },
+        email: {
+            type: String,
+            default: null
+        },
+        whatsapp: {
+            type: String,
+            default: null
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../assets/scss/components/card';
+</style>
